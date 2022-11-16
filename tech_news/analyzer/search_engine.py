001b0e326617cf3a12e4374c1c01c12f7ad7f9a2
Eric Santos <eric.silvasantos44@gmail.com>
2022-11-16 11:00:06 -0300

    nono requisito

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -29,4 +29,6 @@ def search_by_tag(tag):
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    news = search_news({"category": {"$regex": category, "$options": "i"}})
+    list_news = [(new["title"], new["url"]) for new in news]
+    return list_news
