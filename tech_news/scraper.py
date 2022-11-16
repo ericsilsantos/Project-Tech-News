eb355742556f5d36011cc7ed487ec594cbacbed1
Eric Santos <eric.silvasantos44@gmail.com>
2022-11-16 09:15:40 -0300

    quarto requisito

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -28,7 +28,28 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    url = selector.css("link[rel=canonical]::attr(href)").get()
+    title = selector.css(".entry-title::text").get().strip()
+    timestamp = selector.css(".meta-date::text").get()
+    writer = selector.css("a.url::text").get()
+    comments_count = selector.css(
+        ".post-comments h5.title-block::text"
+        ).get() or 0
+    summary = selector.xpath("string(//p)").get().strip()
+    tags = selector.css(".post-tags a::text").getall()
+    category = selector.css(".meta-category span.label::text").get()
+
+    return {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "comments_count": comments_count,
+        "summary": summary,
+        "tags": tags,
+        "category": category,
+    }
 
 
 # Requisito 5
